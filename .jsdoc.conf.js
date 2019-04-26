7ef7a8d7bb65598fc64f60328d69623d39dde57a
Jibin <jibinmathews7@gmail.com>
2019-04-26 20:31:00 +0530

    update docs format and instance methods

--- .jsdoc.conf.js
+++ .jsdoc.conf.js
@@ -16,15 +16,55 @@ module.exports = {
   sourceType: 'module',
   tags: {
     allowUnknownTags: true,
-    dictionaries: ['jsdoc', 'closure'],
+    dictionaries: ['jsdoc'],
   },
   templates: {
     cleverLinks: false,
-    monospaceLinks: false,
+    monospaceLinks: true,
   },
   opts: {
-    template: 'node_modules/postman-jsdoc-theme',
+    template: 'node_modules/docdash',
     destination: './docs/',
     readme: './README.md',
+  },
+  docdash: {
+    static: true,
+    sort: true,
+    sectionOrder: ['Classes', 'Modules', 'Externals', 'Events', 'Namespaces', 'Mixins', 'Tutorials', 'Interfaces'],
+    disqus: 'https://forum.blockcluster.io/',
+    openGraph: {
+      title: '',
+      type: 'website',
+      image: '',
+      site_name: '',
+      url: '',
+    },
+    meta: {
+      title: 'Blockcluster NodeJS SDK',
+      description: '',
+      keyword: '',
+    },
+    search: true,
+    collapse: true,
+    wrap: true,
+    typedefs: true,
+    navLevel: 3,
+    private: true,
+    removeQuotes: 'trim',
+    scripts: [],
+    menu: {
+      'Project Website': {
+        href: 'https://www.blockcluster.io/',
+        target: '_blank',
+        class: 'menu-item',
+        id: 'website_link',
+      },
+      Forum: {
+        href: 'https://forum.blockcluster.io/',
+        target: '_blank',
+        class: 'menu-item',
+        id: 'forum_link',
+      },
+    },
   },
 };
